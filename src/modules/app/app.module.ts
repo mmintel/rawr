a0a8b28d60100ada52b97a83772da029238b53f3
Marc Mintel <marc.mintel@trustedshops.com>
2021-02-08 10:59:15 +0100

    update

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'src/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [SharedModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeOrmConfig'),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
